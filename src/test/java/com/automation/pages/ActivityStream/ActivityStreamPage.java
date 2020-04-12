1ac2b5ac5ed6ff90ef4d9be7410a1c0cd1eec6ff
Omer S <omersolak1986@gmail.com>
2020-04-12 16:25:17 -0500

    BrowserUtilities has changed. AbstractBasePage and AbstractBaseTest revised. MainPage, LoginPage and LoginTests added. TopicIconTest from user_story#1_8 added.

--- src/test/java/com/automation/pages/ActivityStream/ActivityStreamPage.java
+++ src/test/java/com/automation/pages/ActivityStream/ActivityStreamPage.java
@@ -1,11 +1,10 @@
-package com.automation.pages.main;
+package com.automation.pages.ActivityStream;
 
 import com.automation.pages.AbstractBasePage;
-import com.automation.utilities.BrowserUtilities;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class ActivityStream extends AbstractBasePage {
+public class ActivityStreamPage extends AbstractBasePage {
 
     @FindBy(xpath = "//tbody//span[text()='Message']")
     private WebElement sendMessage;
--- src/test/java/com/automation/tests/ActivityStreamPageTests.java
+++ src/test/java/com/automation/tests/ActivityStreamPageTests.java
@@ -2,14 +2,15 @@ package com.automation.tests;
 
 
 import com.automation.pages.LoginPage;
-import com.automation.pages.main.ActivityStream;
+import com.automation.pages.ActivityStream.ActivityStreamPage;
 import com.automation.utilities.BrowserUtilities;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class TopicIconTest extends AbstractBaseTest {
+public class ActivityStreamPageTests extends AbstractBaseTest {
     LoginPage loginPage = new LoginPage();
-    ActivityStream activityStream = new ActivityStream();
+    ActivityStreamPage activityStreamPage = new ActivityStreamPage();
+
     @Test
     public void topicIcon_test(){
         extentTest = extentReports.createTest("Verify Post Topic");
@@ -18,11 +19,11 @@ public class TopicIconTest extends AbstractBaseTest {
         BrowserUtilities.wait(3);
         //System.out.println(driver.getTitle());
         //driver.findElement(By.xpath("//tbody//span[text()='Message']")).click();
-        activityStream.clickToSendMessage();
+        activityStreamPage.clickToSendMessage();
         BrowserUtilities.wait(3);
-        activityStream.clickToTopicIcon();
+        activityStreamPage.clickToTopicIcon();
         BrowserUtilities.wait(3);
-        String topicTitle = activityStream.getTopicInTitleText();
+        String topicTitle = activityStreamPage.getTopicInTitleText();
         System.out.println("topicTitle = " + topicTitle);
         Assert.assertEquals(topicTitle,"Topic");
         extentTest.pass("Topic Title verified");
